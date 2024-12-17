a49e025f9f864ac7031c2d9a17832aa113312f74
Anton.Lezhnin <Anton.Lezhnin@x5.ru>
2024-12-17 13:01:18 +0300

    hw01 Initial commit

--- hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,16 +1,36 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
+
+import java.util.stream.IntStream;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
+    public static final String ANSWER_FORMAT = "%d) %s";
+
     private final IOService ioService;
+
+    private final QuestionDao questionDao;
 
     @Override
     public void executeTest() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
         // Получить вопросы из дао и вывести их с вариантами ответов
+        questionDao.findAll().forEach(this::printQuestion);
+    }
+
+    private void printQuestion(Question question) {
+        ioService.printLine(question.text());
+        IntStream.range(0, question.answers().size())
+            .forEach(index -> printAnswer(question, index));
+        ioService.printLine("");
+    }
+
+    private void printAnswer(Question question, int index) {
+        ioService.printLine(String.format(ANSWER_FORMAT, index + 1, question.answers().get(index).text()));
     }
 }
