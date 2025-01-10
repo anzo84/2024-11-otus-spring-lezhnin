8f6512e6ac5e972266aae6bb1b2e5331c814c697
Anton.Lezhnin <Anton.Lezhnin@x5.ru>
2025-01-10 14:16:45 +0300

    hw02 fix after review

--- hw02-annotation-config/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
+++ hw02-annotation-config/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -14,52 +14,51 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
-
 @ExtendWith(MockitoExtension.class)
 public class CsvQuestionDaoTest {
 
-  QuestionDao subject;
+    private QuestionDao questionDao;
 
-  @Mock
-  TestFileNameProvider fileNameProvider;
+    @Mock
+    private TestFileNameProvider fileNameProvider;
 
-  @BeforeEach
-  public void setUp() {
-    when(fileNameProvider.getTestFileName()).thenReturn("questions.csv");
-    subject = new CsvQuestionDao(fileNameProvider);
-  }
-
-  @Test
-  public void testFindAll() {
-    List<Question> questions = subject.findAll();
-    assertThat(questions)
-        .hasSize(4)
-        .map(x -> x.answers().size())
-        .containsSequence(3, 4, 3, 2);
-
-    assertThat(questions)
-        .flatMap(Question::answers)
-        .map(Answer::text)
-        .containsSequence("Q1-A1", "Q1-A2", "Q1-A3",
-            "Q2-A1", "Q2-A2", "Q2-A3", "Q2-A4",
-            "Q3-A1", "Q3-A2", "Q3-A3",
-            "Q4-A1", "Q4-A2");
-
-    assertThat(questions)
-        .map(Question::text)
-        .containsSequence("Question 1", "Question 2", "Question 3", "Question 4");
-
-    assertThat(questions)
-        .map(this::getIndexOfTrueAnswer)
-        .containsSequence(0, 1, 2, 0);
-  }
-
-  private int getIndexOfTrueAnswer(Question questions) {
-    for (int i = 0; i < questions.answers().size(); i++) {
-      if (questions.answers().get(i).isCorrect()) {
-        return i;
-      }
+    @BeforeEach
+    public void setUp() {
+        when(fileNameProvider.getTestFileName()).thenReturn("questions.csv");
+        questionDao = new CsvQuestionDao(fileNameProvider);
     }
-    return -1;
-  }
+
+    @Test
+    public void testFindAll() {
+        List<Question> questions = questionDao.findAll();
+        assertThat(questions)
+            .hasSize(4)
+            .map(x -> x.answers().size())
+            .containsSequence(3, 4, 3, 2);
+
+        assertThat(questions)
+            .flatMap(Question::answers)
+            .map(Answer::text)
+            .containsSequence("Q1-A1", "Q1-A2", "Q1-A3",
+                "Q2-A1", "Q2-A2", "Q2-A3", "Q2-A4",
+                "Q3-A1", "Q3-A2", "Q3-A3",
+                "Q4-A1", "Q4-A2");
+
+        assertThat(questions)
+            .map(Question::text)
+            .containsSequence("Question 1", "Question 2", "Question 3", "Question 4");
+
+        assertThat(questions)
+            .map(this::getIndexOfTrueAnswer)
+            .containsSequence(0, 1, 2, 0);
+    }
+
+    private int getIndexOfTrueAnswer(Question questions) {
+        for (int i = 0; i < questions.answers().size(); i++) {
+            if (questions.answers().get(i).isCorrect()) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
