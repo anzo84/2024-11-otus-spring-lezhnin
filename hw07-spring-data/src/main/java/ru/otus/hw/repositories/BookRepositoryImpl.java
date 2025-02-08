d804cc867fa4666757e8c6af53158acde6666773
Anton.Lezhnin <Anton.Lezhnin@x5.ru>
2025-02-08 19:50:59 +0300

    hw07 Initial commit

---
+++ hw07-spring-data/src/main/java/ru/otus/hw/repositories/BookRepositoryImpl.java
@@ -0,0 +1,50 @@
+package ru.otus.hw.repositories;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.stereotype.Repository;
+import ru.otus.hw.models.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class BookRepositoryImpl implements BookRepository {
+
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    @Override
+    public Optional<Book> findById(long id) {
+        return Optional.ofNullable(entityManager.createQuery("FROM Book WHERE id = :id", Book.class)
+            .setParameter("id", id)
+            .setHint(EntityGraph.EntityGraphType.FETCH.getKey(),
+                entityManager.getEntityGraph(Book.BOOKS_AUTHOR_GENRES_GRAPH))
+            .getSingleResult());
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return entityManager.createQuery("FROM Book", Book.class)
+            .setHint(EntityGraph.EntityGraphType.FETCH.getKey(),
+                entityManager.getEntityGraph(Book.BOOKS_AUTHOR_GENRES_GRAPH))
+            .getResultList();
+    }
+
+    @Override
+    public Book save(Book book) {
+        if (book.getId() == 0) {
+            entityManager.persist(book);
+            return book;
+        }
+        return entityManager.merge(book);
+    }
+
+    @Override
+    public void deleteById(long id) {
+        findById(id).ifPresent(entityManager::remove);
+    }
+}
---
+++ hw07-spring-data/src/main/java/ru/otus/hw/repositories/CommentRepositoryImpl.java
@@ -0,0 +1,51 @@
+package ru.otus.hw.repositories;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.stereotype.Repository;
+import ru.otus.hw.models.Comment;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentRepositoryImpl implements CommentRepository {
+
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    @Override
+    public Optional<Comment> findById(long id) {
+        return Optional.ofNullable(entityManager.createQuery("FROM Comment WHERE id = :id", Comment.class)
+            .setParameter("id", id)
+            .setHint(EntityGraph.EntityGraphType.FETCH.getKey(),
+                entityManager.getEntityGraph(Comment.COMMENT_BOOKS))
+            .getSingleResult());
+    }
+
+    @Override
+    public List<Comment> findAllCommentsByBookId(long bookId) {
+        return entityManager.createQuery("FROM Comment cm WHERE cm.book.id = :bookId", Comment.class)
+            .setParameter("bookId", bookId)
+            .setHint(EntityGraph.EntityGraphType.FETCH.getKey(),
+                entityManager.getEntityGraph(Comment.COMMENT_BOOKS))
+            .getResultList();
+    }
+
+    @Override
+    public Comment save(Comment comment) {
+        if (comment.getId() == 0) {
+            entityManager.persist(comment);
+            return comment;
+        }
+        return entityManager.merge(comment);
+    }
+
+    @Override
+    public void deleteById(long commentId) {
+        findById(commentId).ifPresent(entityManager::remove);
+    }
+}
