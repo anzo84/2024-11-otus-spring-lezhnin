0b051bbf33fcd45fcb0cd9c0917c20c0bbf8d039
Anton.Lezhnin <Anton.Lezhnin@x5.ru>
2025-02-15 22:27:39 +0300

    hw08 Solution v3

--- hw08-mongodb/src/test/java/ru/otus/hw/repositories/BookRepositoryTest.java
+++ hw08-mongodb/src/test/java/ru/otus/hw/repositories/BookRepositoryTest.java
@@ -1,6 +1,5 @@
 package ru.otus.hw.repositories;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
--- hw08-mongodb/src/test/java/ru/otus/hw/services/GenreServiceTest.java
+++ hw08-mongodb/src/test/java/ru/otus/hw/services/GenreServiceTest.java
@@ -4,11 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import ru.otus.hw.models.Genre;
 
 import static org.assertj.core.api.Assertions.assertThat;
